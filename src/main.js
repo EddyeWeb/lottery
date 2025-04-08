d335baa449814f47ff7582bf9ea845230849ec3d
pszohatzky <peter.szohatzky@cursorinsight.com>
2025-04-08 13:50:49 +0200

    Update project with sass

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+import './assets/tailwind.css'
+import './assets/styles.scss'
 
 createApp(App).mount('#app')
